ad08e1e159e5361210ba2b3572213aec5760c34a
Aljesh-basnet <58818892+Aljesh-basnet@users.noreply.github.com>
2020-04-26 10:39:04 +0545

    login

--- Coursework/app/src/main/java/com/example/coursework/Login.java
+++ Coursework/app/src/main/java/com/example/coursework/Login.java
@@ -2,13 +2,49 @@ package com.example.coursework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-
+    private EditText input_userName, input_password;
+    private Button btn,btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        input_userName=findViewById(R.id.login_username);
+        input_password=findViewById(R.id.login_password);
+        btn=findViewById(R.id.login);
+
+        btn1=findViewById(R.id.createAccount);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username= input_userName.getText().toString();
+                String password= input_password.getText().toString();
+                if (username.isEmpty()) {
+                    Toast.makeText(Login.this, "UserName is empty", Toast.LENGTH_SHORT).show();
+
+                } else if (password.isEmpty()) {
+                    Toast.makeText(Login.this, "Password is empty", Toast.LENGTH_LONG).show();
+                } else if (username.equals("admin") && password.equals("root")) {
+                    startActivity(new Intent(Login.this, MainActivity.class));
+                } else {
+                    Toast.makeText(Login.this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent= new Intent(getApplicationContext(), Signup.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
--- Coursework/app/src/main/java/com/example/coursework/MainActivity.java
+++ Coursework/app/src/main/java/com/example/coursework/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.coursework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.test);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent=new Intent(getApplicationContext(),Login.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
